b09774193ee774d347ef001cd58aac192cbfac22
FMorschel <52160996+FMorschel@users.noreply.github.com>
2023-12-10 18:06:33 -0300

    Exercicio 04

--- src/main.c
+++ src/main.c
@@ -1,62 +1,95 @@
 /*
-Altere o exercício 1 para que a frequência da piscada seja
-função de uma entrada E. 
-O led é ativo em HIGH e conectado ao PB0.
-Quando E estiver em nivel alto, o led pisca a 2 Hz, caso contrário pisca a 7 Hz.  
-A entrada E será feita através do pino PC1. 
-Utilize o TIM10.
+Monte dois conjuntos de 3 leds, um verde, um amarelo e um vermelho, nas
+posições de um semáforo (se não tiveres as cores, não importa.).
+
+Desenvolva um programa de controle para este semáforo de dois tempos. 
+O tempo de verde/vermelho deve ser de 700 ms e 
+o tempo de amarelo aceso é 200ms. 
+
+Faça a sequência 	Vd →	 Vd/Am → 	Vm
+					500ms	 200ms		700ms
 */
 
 /* Includes */
 #include "stm32f4xx.h"
 
 /* Private macro */
-#define F14HZ 1142
-#define F4HZ 4000
+/*typedef struct {
+	uint32_t vermelho;
+	uint32_t amarelo;
+	uint32_t verde;
+} semaforo_t;*/
+#define VERMELHO1 GPIO_ODR_ODR_0
+#define AMARELO1 GPIO_ODR_ODR_1
+#define VERDE1 GPIO_ODR_ODR_2
+#define VERMELHO2 GPIO_ODR_ODR_3
+#define AMARELO2 GPIO_ODR_ODR_4
+#define VERDE2 GPIO_ODR_ODR_5
+
 /* Private variables */
+uint8_t contador = 0;
+/*const semaforo_t semaforo1 = {
+	.vermelho = GPIO_ODR_ODR_0,
+	.amarelo = GPIO_ODR_ODR_1,
+	.verde = GPIO_ODR_ODR_2,
+};
+const semaforo_t semaforo2 = {
+	.vermelho = GPIO_ODR_ODR_3,
+	.amarelo = GPIO_ODR_ODR_4,
+	.verde = GPIO_ODR_ODR_5,
+};*/
 /* Private function prototypes */
 /* Private functions */
 
 int main(void)
 {
-
 	//Ligando clocks dos perifericos
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN | RCC_AHB1ENR_GPIOBEN;
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
 	//RCC->APB1ENR |= RCC_APB1ENR_TIM2EN; <- Outros timers
 	RCC->APB2ENR |= RCC_APB2ENR_TIM10EN;
 
-	// Definindo pino como chave
-	GPIOC->MODER &= ~GPIO_MODER_MODER1;
+	// Configurando pinos para saida
+	GPIOB->MODER &= ~(GPIO_MODER_MODER0 | GPIO_MODER_MODER1 | GPIO_MODER_MODER2
+						| GPIO_MODER_MODER3 | GPIO_MODER_MODER4 | GPIO_MODER_MODER5);
+	GPIOB->MODER |= (GPIO_MODER_MODER0_0 | GPIO_MODER_MODER1_0 | GPIO_MODER_MODER2_0
+						| GPIO_MODER_MODER3_0 | GPIO_MODER_MODER4_0 | GPIO_MODER_MODER5_0);
 
-	// Configurando pino para saida
-	GPIOB->MODER &= ~GPIO_MODER_MODER0;
-	GPIOB->MODER |= GPIO_MODER_MODER0_0;
-
-	// Configurando o timer para 14Hz
+	// Configurando o timer para 10Hz
 	TIM10->PSC = 999;
-	TIM10->ARR = F14HZ;
+	TIM10->ARR = 1599;
 	TIM10->CR1 = TIM_CR1_CEN;
+
+
 
 	while (1)
 	{
-
-		if ((GPIOB->IDR & GPIO_IDR_IDR0) == 0) {
-			TIM10->ARR = F14HZ;
-		} else {
-			TIM10->ARR = F4HZ;
-		}
-
-		if (TIM10->CNT >= TIM10->ARR) {
-			TIM10->CNT = 0;
-		}
-
 		if (TIM10->SR & TIM_SR_UIF) {
 			TIM10->SR &= ~TIM_SR_UIF;
+			contador++;
 
-			if (GPIOC->ODR & GPIO_ODR_ODR0) {
-				GPIOC->ODR &= ~GPIO_ODR_ODR0; //0001000X & 11111110 = 00010000
-			} else {
-				GPIOC->ODR |= GPIO_ODR_ODR0;  //0001000X | 00000001 = 00010001
+			if (contador == 5) {
+				if (GPIOB->ODR & VERDE1) {
+					GPIOB->ODR |= AMARELO1;
+				} else {
+					GPIOB->ODR |= AMARELO2;
+				}
+			} else if (contador == 7) {
+				if (GPIOB->ODR & VERDE1) {
+					GPIOB->ODR |= VERMELHO1;
+					GPIOB->ODR &= ~VERDE1;
+					GPIOB->ODR &= ~AMARELO1;
+
+					GPIOB->ODR |= VERDE2;
+					GPIOB->ODR &= ~VERMELHO2;
+				} else {
+					GPIOB->ODR |= VERMELHO2;
+					GPIOB->ODR &= ~VERDE2;
+					GPIOB->ODR &= ~AMARELO2;
+
+					GPIOB->ODR |= VERDE1;
+					GPIOB->ODR &= ~VERMELHO1;
+				}
+				contador = 0;
 			}
 		}
 	}
