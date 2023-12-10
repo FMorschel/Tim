04ea9d5115d63f4b18fc53ad8ec3c780187e4cf7
FMorschel <52160996+FMorschel@users.noreply.github.com>
2023-12-10 17:23:11 -0300

    Ex_02 exemplo 1

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 /*
-Este é o clássico dos clássicos. Elabore um programa capaz de piscar o Led
-com um ciclo de trabalho de 50% (metade do tempo ligado, metade
-desligado) e com período de 200ms (5 Hz ou 5 piscadas/segundo). 
-O led é ativo em LOW e conectado ao PC0. 
-Utilize o TIM11.
+Altere o exercício 1 para que a frequência da piscada seja
+função de uma entrada E. 
+O led é ativo em HIGH e conectado ao PB0.
+Quando E estiver em nivel alto, o led pisca a 2 Hz, caso contrário pisca a 7 Hz.  
+A entrada E será feita através do pino PC1. 
+Utilize o TIM10.
 */
 
 /* Includes */
@@ -11,6 +12,8 @@ Utilize o TIM11.
 
 /* Private macro */
 /* Private variables */
+uint8_t contador = 0;
+uint8_t limite = 2;
 /* Private function prototypes */
 /* Private functions */
 
@@ -19,27 +22,36 @@ int main(void)
 
 	//Ligando clocks dos perifericos
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN | RCC_AHB1ENR_GPIOBEN;
-	RCC->APB2ENR |= RCC_APB2ENR_TIM11EN;
+	RCC->APB2ENR |= RCC_APB2ENR_TIM10EN;
 
-	// Definindo pino como saida
-	GPIOC->MODER &= ~GPIO_MODER_MODER0;
-	GPIOC->MODER |= GPIO_MODER_MODER0_0;
+	// Definindo pino como chave
+	GPIOC->MODER &= ~GPIO_MODER_MODER1;
 
-	// Configurando pino para chave
+	// Configurando pino para saida
 	GPIOB->MODER &= ~GPIO_MODER_MODER0;
+	GPIOB->MODER |= GPIO_MODER_MODER0_0;
 
-	// Configurando o timer para 10Hz
-	TIM11->PSC = 999;
-	TIM11->ARR = 1599;
-	TIM11->CR1 = TIM_CR1_CEN;
+	// Configurando o timer para 28Hz
+	TIM10->PSC = 999;
+	TIM10->ARR = 570;
+	TIM10->CR1 = TIM_CR1_CEN;
 
 	while (1)
 	{
 
 		if ((GPIOB->IDR & GPIO_IDR_IDR0) == 0) {
-			if (TIM11->SR & TIM_SR_UIF) {
-				TIM11->SR &= ~TIM_SR_UIF;
+			limite = 2;
+		} else {
+			limite = 7;
+		}
 
+		if (TIM10->SR & TIM_SR_UIF) {
+			TIM10->SR &= ~TIM_SR_UIF;
+			contador++;
+
+			if (contador == limite) contador = 0;
+
+			if (contador == 0) {
 				if (GPIOC->ODR & GPIO_ODR_ODR0) {
 					GPIOC->ODR &= ~GPIO_ODR_ODR0; //0001000X & 11111110 = 00010000
 				} else {
@@ -47,6 +59,5 @@ int main(void)
 				}
 			}
 		}
-
 	}
 }
