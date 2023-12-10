9e7651548e70643eb3eb2427faa35b9d7208ca70
FMorschel <52160996+FMorschel@users.noreply.github.com>
2023-12-10 16:13:48 -0300

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,16 @@
+/* Includes */
+#include "stm32f4xx.h"
+
+/* Private macro */
+/* Private variables */
+/* Private function prototypes */
+/* Private functions */
+
+int main(void)
+{
+
+	while (1)
+	{
+
+	}
+}
